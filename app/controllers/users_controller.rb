c22ef6af833a26234f529c3ef20ebda4dfb2f5a2
Keshav Chakravarthy <keshav1985@gmail.com>
2021-11-29 00:18:56 +0530

    Sign in form with json responses from server

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
   def create
+    # user = User.create!(user_params)
+    # if user
+    #   render json: user
+    # else
+    #   # render json: user.errors
+    #   render plain: "die"
+    # end
+    begin
+      user = User.create!(user_params)
+    rescue ActiveRecord::RecordInvalid
+      render json: { message: 'Invalid input' }, status: 400
+    rescue ActiveRecord::ActiveRecordError
+      render json: { message: 'Something went wrong' }, status: 500
+    else
+      render json: { message: 'Great success' }, status: 200
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
--- app/javascript/pages/Signup.jsx
+++ app/javascript/pages/Signup.jsx
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Signup = () => <h1>Sign up Page</h1>;
+const Signup = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPass, setEnteredPass] = useState("");
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const passChangeHandler = (event) => {
+    setEnteredPass(event.target.value);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    const url = "/users";
+    const body = JSON.stringify({
+      email: enteredEmail,
+      password: enteredPass
+    });
+    let response = await fetch(url,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": token
+      },
+      body: body
+    })
+    console.log(response);
+    const data = await response.json();
+    if(response.ok) {      
+      console.log("it worked");
+      console.log(data);
+    } 
+    else {
+      console.log("something went wrong");
+      console.log(data);
+    }
+
+
+    setEnteredEmail('');
+    setEnteredPass('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+    <div>
+      <label htmlFor="user_email">Your Email</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={emailChangeHandler}
+        value={enteredEmail}/>
+    </div>
+    <div>
+      <label htmlFor="user_password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={passChangeHandler}
+        value={enteredPass}/>
+    </div>
+    <div>
+      <button type="submit">Register</button>
+    </div>
+  </form>      
+  );
+};
+
+
 
 export default Signup;
