e5b93e882218fde12db0634d4fb8aea653e2bbdf
Keshav Chakravarthy <keshav1985@gmail.com>
2021-11-28 11:55:48 +0530

    setup basic client side routing

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from "../routes/Home";
+import Signup from "../routes/Signup";
+import Signin from "../routes/Signin";
 
 const App = (props) => (
-<h1>The App {props.name}</h1>
+  <>
+    <h1>The App {props.name}</h1>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign in</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/signin">
+          <Signin />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
