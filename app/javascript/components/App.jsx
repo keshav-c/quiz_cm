a2c23414f2df42418977760b999922ea394fd692
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-04 10:13:43 +0530

    Align items in Header

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
 import QuizPage from "../pages/QuizPage";
+import { Paper, Container } from "@mui/material";
 
 let token = "";
 
@@ -17,27 +18,29 @@ const App = () => {
   }
 
   return (
-    <>
-      <AppHeader
-        loggedIn={loggedIn}
-        authToken={token}
-        onRxUserData={updateUserState}
-      />
-      <Switch>
-        <Route exact path="/">
-          <Home loggedIn={loggedIn} authToken={token} />
-        </Route>
-        <Route exact path="/signup">
-          <Signup loggedIn={loggedIn} onRxUserData={updateUserState} />
-        </Route>
-        <Route exact path="/signin">
-          <Signin loggedIn={loggedIn} onRxUserData={updateUserState} />
-        </Route>
-        <Route exact path="/quiz/:quizId">
-          <QuizPage />
-        </Route>
-      </Switch>
-    </>
+    <Container maxWidth="md">
+      <Paper>
+        <AppHeader
+          loggedIn={loggedIn}
+          authToken={token}
+          onRxUserData={updateUserState}
+        />
+        <Switch>
+          <Route exact path="/">
+            <Home loggedIn={loggedIn} authToken={token} />
+          </Route>
+          <Route exact path="/signup">
+            <Signup loggedIn={loggedIn} onRxUserData={updateUserState} />
+          </Route>
+          <Route exact path="/signin">
+            <Signin loggedIn={loggedIn} onRxUserData={updateUserState} />
+          </Route>
+          <Route exact path="/quiz/:quizId">
+            <QuizPage />
+          </Route>
+        </Switch>
+      </Paper>
+    </Container>
   );
 };
 
--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,49 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logout from "./LogoutButton";
-import { AppBar, Container, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 
 const AppHeader = (props) => (
   <AppBar position="static">
-    <Container maxWidth="xl">
-      <Toolbar>
-        <Typography variant="h6">Create a Quiz</Typography>
+    <Toolbar sx={{ justifyContent: 'flex-end' }}>
+      <Typography variant="h6" sx={{ marginRight: 'auto' }}>Create a Quiz</Typography>
+      <Button
+        component={Link}
+        variant="text"
+        color="inherit"
+        to="/"
+      >
+        Home
+      </Button>
+      {!props.loggedIn && (
         <Button
           component={Link}
           variant="text"
           color="inherit"
-          to="/"
+          to="/signup"
         >
-          Home
+          Sign Up
         </Button>
-        {!props.loggedIn && (
-          <Button
-            component={Link}
-            variant="text"
-            color="inherit"
-            to="/signup"
-          >
-            Sign Up
-          </Button>
-        )}
-        {!props.loggedIn && (
-          <Button
-            component={Link}
-            variant="text"
-            color="inherit"
-            to="/signin"
-          >
-            Sign in
-          </Button>
-        )}
-        {props.loggedIn && (
-          <Logout
-            authToken={props.authToken}
-            onLogoutSuccess={props.onRxUserData}
-          />
-        )}
-      </Toolbar>
-    </Container>
+      )}
+      {!props.loggedIn && (
+        <Button
+          component={Link}
+          variant="text"
+          color="inherit"
+          to="/signin"
+        >
+          Sign in
+        </Button>
+      )}
+      {props.loggedIn && (
+        <Logout
+          authToken={props.authToken}
+          onLogoutSuccess={props.onRxUserData}
+        />
+      )}
+    </Toolbar>
   </AppBar>
 );
 
