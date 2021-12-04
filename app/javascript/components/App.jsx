905b68906b610330604dc57dc9b9b1510e711d4a
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-04 13:08:27 +0530

    Margins on app content

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -5,7 +5,7 @@ import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
 import QuizPage from "../pages/QuizPage";
-import { Paper, Container } from "@mui/material";
+import { Paper, Container, Card } from "@mui/material";
 
 let token = "";
 
@@ -19,26 +19,28 @@ const App = () => {
 
   return (
     <Container maxWidth="md">
-      <Paper>
+      <Paper sx={{ height: '100vh' }}>
         <AppHeader
           loggedIn={loggedIn}
           authToken={token}
           onRxUserData={updateUserState}
         />
-        <Switch>
-          <Route exact path="/">
-            <Home loggedIn={loggedIn} authToken={token} />
-          </Route>
-          <Route exact path="/signup">
-            <Signup loggedIn={loggedIn} onRxUserData={updateUserState} />
-          </Route>
-          <Route exact path="/signin">
-            <Signin loggedIn={loggedIn} onRxUserData={updateUserState} />
-          </Route>
-          <Route exact path="/quiz/:quizId">
-            <QuizPage />
-          </Route>
-        </Switch>
+        <Card sx={{ margin: '2em', padding: '2em', elevation: '4' }}>
+          <Switch>
+            <Route exact path="/">
+              <Home loggedIn={loggedIn} authToken={token} />
+            </Route>
+            <Route exact path="/signup">
+              <Signup loggedIn={loggedIn} onRxUserData={updateUserState} />
+            </Route>
+            <Route exact path="/signin">
+              <Signin loggedIn={loggedIn} onRxUserData={updateUserState} />
+            </Route>
+            <Route exact path="/quiz/:quizId">
+              <QuizPage />
+            </Route>
+          </Switch>
+        </Card>
       </Paper>
     </Container>
   );
